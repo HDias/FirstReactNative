4765df6a96d11c84134891c6aa24ddd8d9999ced
Horecio Araújo Dias <horecio@gmail.com>
2016-03-31 17:25:53 -0300

    Add and Remove Tasks

--- index.android.js
+++ index.android.js
@@ -19,48 +19,152 @@ var Firebase = require('firebase');
 class devdactic_react extends Component {
   constructor(props) {
     super(props);
-    var myFirebaseRef = new Firebase("https://resplendent-fire-5136.firebaseio.com/")
+    var myFirebaseRef = new Firebase("https://resplendent-fire-5136.firebaseio.com/");
+    this.itemsRef = myFirebaseRef.child('items');
+   
+    this.state = {
+      newTodo: '',
+      todoSource: new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
+    };
+   
+    this.items = [];
+  }
 
-    myFirebaseRef.set({
-      title: 'Hello world"',
-      author: 'Horecio'
+  componentDidMount() {
+    // When a todo is added
+    this.itemsRef.on('child_added', (dataSnapshot) => {
+      this.items.push({id: dataSnapshot.key(), text: dataSnapshot.val()});
+      this.setState({
+        todoSource: this.state.todoSource.cloneWithRows(this.items)
+      });
     });
-  };
+   
+    // When a todo is removed
+    this.itemsRef.on('child_removed', (dataSnapshot) => {
+        this.items = this.items.filter((x) => x.id !== dataSnapshot.key());
+        this.setState({
+          todoSource: this.state.todoSource.cloneWithRows(this.items)
+        });
+    });
+  }
+
+  addTodo() {
+    if (this.state.newTodo !== '') {
+      this.itemsRef.push({
+        todo: this.state.newTodo
+      });
+
+      this.setState({
+        newTodo: ''
+      })
+    }
+  }
+
+  removeTodo(rowdData) {
+    this.itemsRef.child(rowdData.id).remove();
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Horecio Araújo Dias
-        </Text>
+      <View style={styles.appContainer}>
+        <View style={styles.titleView}>
+          <Text style={styles.titleText}>
+            My Todos
+          </Text>
+        </View>
+        <View style={styles.inputcontainer}>
+          <TextInput style={styles.input} onChangeText={(text) => this.setState({newTodo: text})} value={this.state.newTodo}/>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => this.addTodo()}
+            underlayColor='#dddddd'>
+            
+            <Text style={styles.btnText}>Add!</Text>
+
+          </TouchableHighlight>
+        </View>
+        <ListView
+          dataSource={this.state.todoSource}
+          renderRow={this.renderRow.bind(this)} />
       </View>
+    );
+  }
+
+  renderRow(rowData) {
+    return (
+      <TouchableHighlight
+        underlayColor='#dddddd'
+        onPress={() => this.removeTodo(rowData)}>
+        <View>
+          <View style={styles.row}>
+            <Text style={styles.todoText}>{rowData.text.todo}</Text>
+          </View>
+          <View style={styles.separator} />
+        </View>
+      </TouchableHighlight>
     );
   }
 };
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer:{
+    flex: 1
+  },
+  titleView:{
+    backgroundColor: '#48afdb',
+    paddingTop: 30,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  titleText:{
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  inputcontainer: {
+    marginTop: 5,
+    padding: 10,
+    flexDirection: 'row'
   },
+  button: {
+    height: 36,
+    flex: 2,
+    flexDirection: 'row',
+    backgroundColor: '#48afdb',
+    justifyContent: 'center',
+    color: '#FFFFFF',
+    borderRadius: 4,
+  },
+  btnText: {
+    fontSize: 18,
+    color: '#fff',
+    marginTop: 6,
+  },
+  input: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48afdb',
+    borderRadius: 4,
+    color: '#48BBEC'
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 12,
+    height: 44
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC',
+  },
+  todoText: {
+    flex: 1,
+  }
 });
 
 AppRegistry.registerComponent('devdactic_react', () => devdactic_react);
